a19f6df4a69f56c4f1e8e37a40b6d95c616f48f4
kong <kongjw0110@gmail.com>
2020-11-04 06:00:29 +0000

    commit

--- SystemProgramming/week09/unix-client.c
+++ SystemProgramming/week09/unix-client.c
@@ -13,48 +13,51 @@
 
 int main(void){
     int sd, len;
-    char s_buf[SIZE];
-    char r_buf[SIZE];
-    char file_path[SIZE];
+    char s_buf[SIZE];   // send buffer
+    char r_buf[SIZE];   // recv buffer
     int n;
 
     struct sockaddr_un ser;
-
+    //socket
     if((sd=socket(AF_UNIX, SOCK_STREAM,0))==-1){
         perror("socket");
         exit(1);
     }
-    
+    //구조체 설정
     memset((char *)&ser,'\0', sizeof(ser));
     ser.sun_family = AF_UNIX;
     strcpy(ser.sun_path,SOCKET_NAME);
     len = sizeof(ser.sun_family) + strlen(ser.sun_path);
     
-    
+    //connect
     if(connect(sd, (struct sockaddr *)&ser, len)){
         perror("connect");
         exit(1);
     }
     while(1){
         printf("\n\nInput File Path : ");
-        gets(s_buf);
-        //strcpy(s_buf,"/home/sp20135103/git/SystemProgramming/test.txt");
+        gets(s_buf); //파일의 정대 경로 입력
+        
+        //서버에게 정대경로 send
         if(send(sd, s_buf, SIZE, 0) == -1){
             perror("send");
             exit(1);
         }
-
+        
+        //서버로부터 파일 내용 rcev
         if((n=recv(sd, r_buf,SIZE,0))==-1){
             perror("recv");
             exit(1);
         }
         
-        while(n>=SIZE){
+        while(n>=SIZE){ //byte초과
+            //출력후 다시 recv
             r_buf[n]='\0';
             printf("%s",r_buf);
             n=recv(sd, r_buf,SIZE,0);
         }
-        r_buf[n]='\0';
+
+        r_buf[n]='\0'; //남은 
         printf("%s",r_buf);
 
     }
--- SystemProgramming/week09/unix-server.c
+++ SystemProgramming/week09/unix-server.c
@@ -13,11 +13,10 @@
 #define SIZE 1024
 
 int main(void){
-    char buf[SIZE];
-    char dir_path[SIZE];
-    char file_name[SIZE];
+    char buf[SIZE];         //파일 내용
+    char dir_path[SIZE];    //디렉토리 경로
+    char file_name[SIZE];   //파일 명
     
-
     DIR *dp;
     struct dirent *dent;
 
@@ -25,28 +24,32 @@ int main(void){
     int sd, nsd, len, clen;
 
     int i, d_index, f_index=0;
-    int n, fd, f_flag=0;
+    int n, fd, f_flag=0; //f_flag : 파일 발견 flag (찾으면 1)
 
+    //socket
     if((sd = socket(AF_UNIX,SOCK_STREAM, 0)) == -1){
         perror("socket");
         exit(1);
     }
-
+    
+    //구조체 설정
     memset((char *)&ser, 0 , sizeof(struct sockaddr_un));
     ser.sun_family = AF_UNIX;
     strcpy(ser.sun_path,SOCKET_NAME);
     len = sizeof(ser.sun_family) + strlen(ser.sun_path);
 
+    //bind
     if(bind(sd, (struct sockaddr*)&ser, len)){
         perror("bind");
         exit(1);
     }
+    //listen
     if(listen(sd, 5) < 0){
         perror("listen");
         exit(1);
     }
     
-        
+    //accet
     if((nsd = accept(sd, (struct sockaddr *)&cli, &clen))==-1){
         perror("accept");
         exit(1);
@@ -54,14 +57,17 @@ int main(void){
     while(1){
         
         printf("Waiting...\n");
+        //recv
         if(recv(nsd, dir_path, sizeof(dir_path),0)==-1){
             perror("recv");
             exit(1);
-        }   
+        }
+        // q 입력 시 반복문 종료
         if(strcmp(dir_path,"q")==0){
             break;
         }
         
+        // 파일 명 앞의 '/'의 index값을 가져옴
         for(i=strlen(dir_path)-1;i>=0;i--){
             if(dir_path[i] == '/'){
                 d_index = i;
@@ -69,11 +75,13 @@ int main(void){
             }
         }
         f_index = 0;
+        // 파일명만 따로 추출
         while(i < strlen(dir_path)){
             file_name[f_index++] = dir_path[i+1];
             i++;
         }
-        dir_path[d_index] = '\0';
+        
+        dir_path[d_index] = '\0'; // 파일명을 제외한 디렉토리 path
         file_name[f_index] = '\0';        
 
         printf("directory path : %s\n",dir_path);
@@ -81,35 +89,37 @@ int main(void){
 
         chdir(dir_path);
         
+        // 디렉토리 open
         if((dp = opendir("."))==NULL){
             perror("opendir");
             exit(1);
         }
 
+        // 디렉토리 내의 파일 정보 가져오기
         while((dent=readdir(dp))){
-            if(strcmp(dent->d_name,file_name)==0){
-                f_flag=1;
-                if((fd=open(file_name,O_RDONLY))==-1){
+            if(strcmp(dent->d_name,file_name)==0){ //파일명 비교
+                f_flag=1; //파일 발견 flag
+                if((fd=open(file_name,O_RDONLY))==-1){ //파일 open
                     perror("oepn");
                     exit(1);
                 }
-                while((n=read(fd,buf,SIZE))>0){
-                    if(send(nsd,buf,n,0)==-1){
+                while((n=read(fd,buf,SIZE))>0){ //파일 내용 read
+                    if(send(nsd,buf,n,0)==-1){ //파일 내용 send
                         perror("send");
                         exit(1);
                     }
                 }
-                close(fd);
+                close(fd); //파일 기술자 close
                 break;
             }
         }
-        if(!f_flag){
+        if(!f_flag){ //파일 발견 flag가 0이면 실행
             if(send(nsd,"File not exist",14,0)==-1){
                 perror("send");
                 exit(1);
             }
         }
-        closedir(dp);
+        closedir(dp); //디렉토리 close
     }
     close(nsd);
     close(sd);
