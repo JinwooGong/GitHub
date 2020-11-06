628b81067c464576687cd19c8c975440ce5ce7e8
kong <kongjw0110@gmail.com>
2020-11-06 01:23:34 +0000

    commit

---
+++ SystemProgramming/week10/ex12-2s.c
@@ -0,0 +1,14 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define PORTNUM 9002
+
+int main(void){
+    
+}
